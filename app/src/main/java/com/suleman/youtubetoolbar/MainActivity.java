f1d88213d50c61b55b493f7ada8926d449c97a9f
sulemankhan447 <sulemankhan447@gmail.com>
2018-11-06 05:22:51 +0530

    Design Completed

--- app/src/main/java/com/suleman/youtubetoolbar/MainActivity.java
+++ app/src/main/java/com/suleman/youtubetoolbar/MainActivity.java
@@ -1,13 +1,29 @@
 package com.suleman.youtubetoolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar)findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
